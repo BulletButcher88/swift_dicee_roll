84c581df2c377ebf7a534779cfb8acc9e9636f0b
Mark Butcher <markjamesbutcher@gmail.com>
2020-12-04 13:27:54 +1100

    first commit

--- DiceSwift/ViewController.swift
+++ DiceSwift/ViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diveViewImage1: UIImageView!
+    @IBOutlet weak var diveViewImage2: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+
+    @IBAction func rollButton(_ sender: UIButton) {
+        diveViewImage1.image = diceArray.randomElement()
+        diveViewImage2.image = diceArray.randomElement()
     }
-
-
 }
 
